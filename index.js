96422b00014ebb15381fe500d6c8769b1fbc63df
012marcal <marcal.jobsdev@gmail.com>
2025-03-12 21:21:43 -0300

    Aula-12-03-2025

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-console.log("Olá mundo") 
+// console.log("Olá mundo") 
 
-var num1 = 210;
-var num2 = 10;
+// var num1 = 210;
+// var num2 = 10;
 
-somar()
+// somar()
 
 
 
-function  somar ()
+// function  somar ()
+// {
+// console.log (num1+num2);
+// }
+
+// function mult(a,b){
+
+//     return a * b
+
+// }
+
+
+// console.log(mult(10,10))
+
+// console.log("Cadastro correto!")
+
+
+
+let sol = false;
+
+if(sol ){
+
+console.log("Vamos ao parque")
+}else
 {
-console.log (num1+num2);
-}
-
-function mult(a,b){
-
-    return a * b
-
+    console.log("Não vamos ao parque")
 }
 
 
-console.log(mult(10,10))
+console.log(typeof NaN)
 
-console.log("Cadastro correto!")
+console.log(`a soma de 50 + 34 = ${50+34}`)
+
+console.log(5*null)
+console.log("5"-2)
+console.log("4"+1)
+console.log("dois" * "tres")
+
+
+
+let x,y;
+
+x = prompt("number um: ")
+y = prompt("number dois: ")
+
+let result = x * y 
+
+console.log(result)

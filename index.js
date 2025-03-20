40722ac3848906d24836db673c7cb014ece1fc16
012marcal <marcal.jobsdev@gmail.com>
2025-03-20 09:45:32 -0300

    Aula 20-03-2024

--- index.js
+++ index.js
@@ -45,11 +45,116 @@ console.log("5"-2)
 console.log("4"+1)
 console.log("dois" * "tres")
 
-function fodase() {
 
-    let a= alert("Num1 : ")
+// 1. console.log()
+console.log("Olá, Mundo!");
 
-    console.log(a)
+// 2. let
+let x = 5;
+x = 10;
+console.log(x);
+
+// 3. const
+const y = 20;
+console.log(y);
+
+// 4. function
+function soma(a, b) {
+    return a + b;
 }
 
-fodase();
+// 5. if/else
+if (x > 10) {
+    console.log("Maior");
+} else {
+    console.log("Menor");
+}
+
+// 6. for
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// 7. document.querySelector()
+document.querySelector("#idElemento").innerHTML = "Novo Conteúdo";
+
+// 8. addEventListener
+button.addEventListener("click", () => {
+    console.log("Clique!");
+});
+
+// 9. Array.prototype.map()
+[1, 2, 3].map(num => num * 2);
+
+// 10. fetch()
+fetch("https://api.exemplo.com")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+// 11. if
+if (x > 10) {
+    console.log("Maior que 10");
+}
+
+// 12. if else
+if (x > 10) {
+    console.log("Maior");
+} else {
+    console.log("Menor ou igual");
+}
+
+// 13. else if
+if (x > 10) {
+    console.log("Maior que 10");
+} else if (x === 10) {
+    console.log("Igual a 10");
+} else {
+    console.log("Menor que 10");
+}
+
+// 14. switch
+switch (dia) {
+    case "segunda":
+        console.log("Trabalhar");
+        break;
+    default:
+        console.log("Descansar");
+}
+
+// 15. while
+while (x < 5) {
+    console.log(x);
+    x++;
+}
+
+// 16. do while
+do {
+    console.log(x);
+    x++;
+} while (x < 5);
+
+// 17. for
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// 18. break
+for (let i = 0; i < 10; i++) {
+    if (i === 5) break;
+    console.log(i);
+}
+
+// 19. continue
+for (let i = 0; i < 5; i++) {
+    if (i === 2) continue;
+    console.log(i);
+}
+
+// 20. Array
+const arr = [1, 2, 3];
+console.log(arr[0]);
+
+// 21. DOM (exemplo com document.querySelector)
+document.querySelector("#idElemento").innerHTML = "Novo Conteúdo";
+
+

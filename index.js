bde7069531602349f1584d722c216cc615723d46
012marcal <marcal.jobsdev@gmail.com>
2025-03-31 13:57:55 -0300

    aula27-03-25

--- index.js
+++ index.js
@@ -1,160 +1,55 @@
-// console.log("Olá mundo") 
+const http = require('http');
 
-// var num1 = 210;
-// var num2 = 10;
-
-// somar()
+const booksController = require('./booksController.js');
 
 
 
-// function  somar ()
-// {
-// console.log (num1+num2);
-// }
+const server = http.createServer((req, res) => {
 
-// function mult(a,b){
-
-//     return a * b
-
-// }
-
-
-// console.log(mult(10,10))
-
-// console.log("Cadastro correto!")
+  const [_, route, id] = req.url.split('/'); // Divide a URL em partes
 
 
 
-let sol = false;
+  if (route === 'books') {
 
-if(sol ){
+    if (req.method === 'GET') {
 
-console.log("Vamos ao parque")
-}else
-{
-    console.log("Não vamos ao parque")
-}
+      booksController.getBooks(res);
 
+    } else if (req.method === 'POST') {
 
-console.log(typeof NaN)
+      booksController.addBook(req, res);
 
-console.log(`a soma de 50 + 34 = ${50+34}`)
+    } else if (req.method === 'PUT' && id) {
 
-console.log(5*null)
-console.log("5"-2)
-console.log("4"+1)
-console.log("dois" * "tres")
+      booksController.updateBook(req, res, id);
 
+    } else if (req.method === 'DELETE' && id) {
 
-// 1. console.log()
-console.log("Olá, Mundo!");
+      booksController.deleteBook(res, id);
 
-// 2. let
-let x = 5;
-x = 10;
-console.log(x);
+    } else {
 
-// 3. const
-const y = 20;
-console.log(y);
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
 
-// 4. function
-function soma(a, b) {
-    return a + b;
-}
+      res.end('Rota não encontrada');
 
-// 5. if/else
-if (x > 10) {
-    console.log("Maior");
-} else {
-    console.log("Menor");
-}
+    }
 
-// 6. for
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+  } else {
 
-// 7. document.querySelector()
-document.querySelector("#idElemento").innerHTML = "Novo Conteúdo";
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
 
-// 8. addEventListener
-button.addEventListener("click", () => {
-    console.log("Clique!");
+    res.end('Rota não encontrada');
+
+  }
+
 });
 
-// 9. Array.prototype.map()
-[1, 2, 3].map(num => num * 2);
-
-// 10. fetch()
-fetch("https://api.exemplo.com")
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-// 11. if
-if (x > 10) {
-    console.log("Maior que 10");
-}
-
-// 12. if else
-if (x > 10) {
-    console.log("Maior");
-} else {
-    console.log("Menor ou igual");
-}
-
-// 13. else if
-if (x > 10) {
-    console.log("Maior que 10");
-} else if (x === 10) {
-    console.log("Igual a 10");
-} else {
-    console.log("Menor que 10");
-}
-
-// 14. switch
-switch (dia) {
-    case "segunda":
-        console.log("Trabalhar");
-        break;
-    default:
-        console.log("Descansar");
-}
-
-// 15. while
-while (x < 5) {
-    console.log(x);
-    x++;
-}
-
-// 16. do while
-do {
-    console.log(x);
-    x++;
-} while (x < 5);
-
-// 17. for
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
-
-// 18. break
-for (let i = 0; i < 10; i++) {
-    if (i === 5) break;
-    console.log(i);
-}
-
-// 19. continue
-for (let i = 0; i < 5; i++) {
-    if (i === 2) continue;
-    console.log(i);
-}
-
-// 20. Array
-const arr = [1, 2, 3];
-console.log(arr[0]);
-
-// 21. DOM (exemplo com document.querySelector)
-document.querySelector("#idElemento").innerHTML = "Novo Conteúdo";
 
 
+server.listen(3000, () => {
+
+  console.log('Servidor rodando na porta 3000');
+
+});
